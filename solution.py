7455623b82bb3c8f21d0af8799544631ae3a499a
grebenshchikova-em <113313451+grebenshchikova-em@users.noreply.github.com>
2023-04-07 20:23:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 586404828 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+  g = []
+  g = []
+  for i in range(len(x)):
+    g.append(2 * x[i] / 4624.)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+#    return x.mean() # Ваш ответ
+    return np.mean(g)
