f41fae2fe4d4bd0467169b354e64c172a0c89958
grebenshchikova-em <113313451+grebenshchikova-em@users.noreply.github.com>
2023-04-07 21:16:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,12 +5,12 @@ import numpy as np
 chat_id = 586404828 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-  g = []
-  g = []
-  for i in range(len(x)):
-    g.append(2 * x[i] / 4624.)
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-#    return x.mean() # Ваш ответ
-    return np.mean(g)
+  t = 68
+  n = len(x)
+  mu = 0
+  sigma = 1
+
+  x_error = np.random.normal(mu, sigma, n)
+  a = 2 * x_error/ t**2
+
+  return a.mean()
